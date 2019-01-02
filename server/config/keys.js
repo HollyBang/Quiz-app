efd46aefc555a279be8f6c4abd430a7e8d669955
Andrey Barchuk <andreyb@123software.ru>
2019-01-02 16:01:19 +0200

    connect to mongo db

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:`mongodb+srv://del-shop__user:zheka1@cluster0-1j0oa.mongodb.net/del-shop_db_n?retryWrites=true`,
+    mongoURI:`mongodb+srv://holy:123321@holycluster-qeddq.mongodb.net/quizdb?retryWrites=true`
 }
